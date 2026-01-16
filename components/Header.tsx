143e9c16cf3774171a0b822731fc61f4663c036e
Raysun <adeniyiraymond31@gmail.com>
2026-01-16 22:57:44 +0100

    fix heading space

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
             <div className="w-10 h-10 bg-blue-600 rounded-lg flex items-center justify-center">
               <GraduationCap className="w-6 h-6 text-white" />
             </div>
-            <div>
+            <div className="max-sm:hidden">
               <h1 className="text-xl font-bold text-blue-900">
                 AAUA Admission
               </h1>
